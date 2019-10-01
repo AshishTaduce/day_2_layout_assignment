23b945e8f93f8c18229275a5d5d17e80c0c74eff
AshishTaduce <ashishteddy2@gmail.com>
2019-10-01 14:31:45 +0530

    Drawing issue...

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  Container container1 = Container(
+    color: Colors.red,
+    height: double.infinity,
+    width: 100,
+  );
+
+  Container container2 = Container(
+    color: Colors.blue,
+    width: 100,
+    height: double.infinity,
+
+  );
+
+  Container container3 = Container(
+    color: Colors.yellow,
+    width: 100,
+    height: 100,
+
+  );
+
+  Container container4 = Container(
+    color: Colors.green,
+    width: 100,
+    height: 100,
+
+  );
+
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(),
+        backgroundColor: Colors.purple,
+        body: Row(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                container1,
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                container3,container4,
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                container2,
+              ],
+            ),
+
+          ],
+        ),
       ),
     ),
   );
